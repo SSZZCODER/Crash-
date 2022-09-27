08149043e850d5a544c971b1cba3672c388c7c7a
eee <patkuanz@gmail.com>
2022-09-27 16:51:27 -0700

    updated main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 # Main File
-import pygame    
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((500,500))
+
+exit = False
+while not exit:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:   
+            exit=True
+
+    
