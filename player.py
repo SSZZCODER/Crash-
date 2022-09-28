876538746da59c8d89fed7dc22b89392f5aabb95
SSZZCODER <samhongmu@gmail.com>
2022-09-27 17:26:20 -0700

    Resized player and changed cords

--- player.py
+++ player.py
@@ -5,5 +5,6 @@ class Player:
     hunger = 100 
 
     playerimage = pygame.image.load('images/New Piskel (5).png')
+    playerimage = pygame.transform.scale(playerimage,(200,200))
     def Render(screen):
-        screen.blit(Player.playerimage,(350,350))
+        screen.blit(Player.playerimage,(275,275))
