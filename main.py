87fe47aacef2a5c188b2fc4ef6151c191207b048
eee <patkuanz@gmail.com>
2022-09-27 17:16:01 -0700

    changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Main File
 import pygame
-
+from player import Player
 pygame.init()
 screen = pygame.display.set_mode((500,500))
 
@@ -10,3 +10,7 @@ while not exit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:   
             exit=True
+
+    Player.Render(screen)
+    
+    pygame.display.flip()
--- player.py
+++ player.py
@@ -3,3 +3,7 @@ import pygame
 class Player:
     health = 100
     hunger = 100 
+
+    playerimage = pygame.image.load('images/New Piskel (5).png')
+    def Render(screen):
+        screen.blit(Player.playerimage,(200,200))
