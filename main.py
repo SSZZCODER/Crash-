e1a50b9925af11f07b4413a40196c91177491538
eee <patkuanz@gmail.com>
2022-09-29 21:30:46 -0700

    movement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Main File
 import pygame
 from player import Player
+
 pygame.init()
 screen = pygame.display.set_mode((750,750))
 
@@ -11,7 +12,8 @@ while not exit:
         if event.type == pygame.QUIT:   
             exit=True
 
+    screen.fill((0,0,0))
     Player.Render(screen)
     Player.Move(event)
-    
+   
     pygame.display.flip()
--- player.py
+++ player.py
@@ -1,18 +1,24 @@
 import pygame
+import time
 
 class Player:
+
+    
     health = 100
     hunger = 100 
-    
-    player_x = 275
-    player_y = 275
+    player_x = 250
+    player_y = 250
 
     playerimage = pygame.image.load('images/New Piskel (5).png')
+
     def Move(self):
        if pygame.key.get_pressed()[pygame.K_LEFT]:
             Player.player_x -= 1
+            
        if pygame.key.get_pressed()[pygame.K_RIGHT]:
             Player.player_x += 1
+            
+
     def Render(screen):
         screen.blit(Player.playerimage,(Player.player_x, Player.player_y))
     playerimage = pygame.transform.scale(playerimage,(200, 200))
