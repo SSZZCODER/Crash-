8504e65ba9e1c71eec356c7a912892763b5c3ae5
eee <patkuanz@gmail.com>
2022-10-04 16:37:05 -0700

    new images and border

--- player.py
+++ player.py
@@ -9,19 +9,19 @@ class Player:
     player_x = 250
     player_y = 250
 
-    playerimage = pygame.image.load('images/New Piskel (5).png')
+    playerimage = pygame.image.load('images/New Piskel (24).png')
 
     def Move(self):
         
        if pygame.key.get_pressed()[pygame.K_s]:
             Player.player_y += 1
-            if Player.player_y > 635:
-                Player.player_y = 635
+            if Player.player_y > 630:
+                Player.player_y = 630
 
        if pygame.key.get_pressed()[pygame.K_w]:
             Player.player_y -= 1
-            if Player.player_y < -85:
-                Player.player_y = -85
+            if Player.player_y < -81.5:
+                Player.player_y = -81.5
         
        if pygame.key.get_pressed()[pygame.K_a]:
             Player.player_x -= 1
@@ -29,8 +29,8 @@ class Player:
                 Player.player_x = -85
        if pygame.key.get_pressed()[pygame.K_d]:
             Player.player_x += 1
-            if Player.player_x > 621:
-                Player.player_x = 621
+            if Player.player_x > 627:
+                Player.player_x = 627
 
 
         
