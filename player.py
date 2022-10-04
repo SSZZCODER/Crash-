f73d82ecb92c22e95bcad7efc12994877a65938c
SSZZCODER <samhongmu@gmail.com>
2022-10-04 16:14:45 -0700

    up down movement

--- player.py
+++ player.py
@@ -13,12 +13,18 @@ class Player:
 
     def Move(self):
         
+       if pygame.key.get_pressed()[pygame.K_s]:
+            Player.player_y += 1
+            if Player.player_y > 635:
+                Player.player_y = 635
 
-    
+       if pygame.key.get_pressed()[pygame.K_w]:
+            Player.player_y -= 1
+            if Player.player_y < -85:
+                Player.player_y = -85
         
        if pygame.key.get_pressed()[pygame.K_a]:
             Player.player_x -= 1
-            
             if Player.player_x < -85:
                 Player.player_x = -85
        if pygame.key.get_pressed()[pygame.K_d]:
