3906fbe6b46685ab47ef3cbb3514292698f6fc9b
eee <patkuanz@gmail.com>
2022-10-11 16:21:01 -0700

    background

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import time
 
 pygame.init()
 screen = pygame.display.set_mode((750,750))
+background = pygame.image.load('images/grass.png')
+background = pygame.transform.scale(background, (750,750))
 
 clock = pygame.time.Clock()
 exit = False
@@ -15,7 +17,7 @@ while not exit:
             exit=True
 
     
-    screen.fill((0,0,0))
+    screen.blit(background, (0,0))
     Player.Update()
     Player.Render(screen)
 
