c8080852b16f3a0a4bb9a279bcf57ea9cf789cb8
eee <patkuanz@gmail.com>
2022-11-23 19:58:28 -0800

    lava

--- lava.py
+++ lava.py
@@ -27,22 +27,30 @@ def main():
     enemy_z1 = zombie(250, 250, 2, 100, 5, 30, 30)
     GameLogic.enemyList.append(enemy_z1)
 
-    StaminaBar = staminabar(0, 0, 115, 20)
-    
+    StaminaBar = staminabar(30, 30, 115, 20)
+    HealthBar = healthbar(30, 0, 115, 20)
+
+
+    heart = pygame.image.load('images/heart.png')
+    heart = pygame.transform.scale(heart, (120, 120))
+
+    energy = pygame.image.load('images/energy.png')
+    energy = pygame.transform.scale(energy, (65, 65))
 
     while not exit:
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:   
                 exit=True
-
             if event.type == pygame.QUIT:
                 return -1
-            
-            screen.blit(lavaImage, (0,0))
-            Player.Update()
-            enemy_z1.update(screen)
-            Player.Render(screen)
-            StaminaBar.render(screen)
-            pygame.display.flip()
-            clock.tick(60)
+        screen.blit(lavaImage, (0,0))
+        Player.Update()
+        enemy_z1.update(screen)
+        Player.Render(screen)
+        StaminaBar.render(screen)
+        HealthBar.render(screen)
+        screen.blit(heart, (-29, -45))
+        screen.blit(energy, (-9, 10))
+        pygame.display.flip()
+        clock.tick(60)
