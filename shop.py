43e90b352cff42ae09a4689cd93fac6c77fc539a
eee <patkuanz@gmail.com>
2023-07-10 19:58:11 -0700

    inventory and shop

--- shop.py
+++ shop.py
@@ -12,13 +12,17 @@ class Shop:
         self.image = pygame.transform.scale(self.image, (98 *7.75,73 * 7.75))
         self.rifle = pygame.image.load("images/rifle.png")
         self.rifle = pygame.transform.scale(self.rifle, (90*2,18*2))
+        self.riflecost = 3
         self.bomb = pygame.image.load("images/bomb.png")
         self.bomb = pygame.transform.scale(self.bomb, (12*12,12*12))
+        self.bombcost = 1
         self.sword = pygame.image.load("images/sword.png")
         self.sword = pygame.transform.scale(self.sword, (17*2 ,64*2))
+        self.swordcost = 2
         self.buy1 = pygame.Rect([78,460], [195,65])
         self.buy2 = pygame.Rect([310, 460], [195,65])
         self.buy3 = pygame.Rect([543,460], [195,65])
+        self.playercoins = 0
     def render(self, screen):
         screen.blit(self.image, (self.xPos, self.yPos))
         screen.blit(self.rifle, (88, 305))
@@ -28,17 +32,39 @@ class Shop:
         screen.blit(self.sword, (613,255))
         #pygame.draw.rect(screen, [250,0,0], self.buy3)
     def buy(self, events):
+        for i in range(Player.playerInventory.slots):
+            if Player.playerInventory.items[i] != None:
+                if Player.playerInventory.items[i].name == "Coin":
+                    self.playercoins = Player.playerInventory.items[i].amount
+                    break
+
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if self.buy1.collidepoint(event.pos):
+                if self.buy1.collidepoint(event.pos) and self.playercoins >= self.riflecost:
                     print("Bought Rifle")
                     Player.playerInventory.addObject(items.Rifle())
-                elif self.buy2.collidepoint(event.pos):
+                    self.playercoins -= self.riflecost
+                elif self.buy1.collidepoint(event.pos) and self.playercoins< self.riflecost:
+                    print("not enough money")
+                if self.buy2.collidepoint(event.pos) and self.playercoins >= self.bombcost:
                     print("Bought Bomb")
                     Player.playerInventory.addItem(items.Bomb())
-                else: 
+                    self.playercoins -= self.bombcost
+                elif self.buy2.collidepoint(event.pos) and self.playercoins< self.bombcost:   
+                    print("not enough money")
+                if self.buy3.collidepoint(event.pos) and self.playercoins >= self.swordcost:
                     print("Bought Sword")
                     Player.playerInventory.addObject(items.Sword())
+                    self.playercoins -= self.swordcost
+                elif self.buy3.collidepoint(event.pos) and self.playercoins< self.swordcost:
+                    print("not enough money")
+
+        for i in range(Player.playerInventory.slots):
+            if Player.playerInventory.items[i] != None:
+                if Player.playerInventory.items[i].name == "Coin":
+                    Player.playerInventory.items[i].amount = self.playercoins
+                    break
+
         
 
     def update(self, screen, events):
