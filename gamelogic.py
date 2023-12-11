72eb47f5026b5c7f64375f9fdb8196eece47295a
Sam Z <114539019+SSZZCODER@users.noreply.github.com>
2023-12-10 17:02:55 -0800

    boss

--- gamelogic.py
+++ gamelogic.py
@@ -4,9 +4,9 @@ class GameLogic:
     #pygame.mixer.pre_init(44100, 16, 2, 4096)
     pygame.mixer.init()
     up = False
-    enemyList = {"grass":[], "lava":[], "ocean":[], "Boss1":[], "Boss2":[], "Boss3":[], "Boss4":[], "Boss5": [], "Boss6": [],"jungle":[], "snow":[], "desert":[]}
-    itemlist = {"grass":[], "lava":[], "ocean":[],"Boss1":[],"Boss2":[], "Boss3":[],"Boss4":[],"Boss5": [],"Boss6": [], "jungle":[], "snow":[], "desert":[]}
-    objects = {"grass":[], "lava":[], "ocean":[], "Boss1":[],"Boss2":[], "Boss3":[], "Boss4":[], "Boss5": [],"Boss6": [],"jungle":[], "snow":[], "desert":[]}
+    enemyList = {"grass":[], "lava":[], "ocean":[], "Boss1":[], "Boss2":[], "Boss3":[], "Boss4":[], "Boss5": [], "Boss6": [],"jungle":[], "snow":[], "desert":[], "Boss7":[]}
+    itemlist = {"grass":[], "lava":[], "ocean":[],"Boss1":[],"Boss2":[], "Boss3":[],"Boss4":[],"Boss5": [],"Boss6": [], "jungle":[], "snow":[], "desert":[], "Boss7":[]}
+    objects = {"grass":[], "lava":[], "ocean":[], "Boss1":[],"Boss2":[], "Boss3":[], "Boss4":[], "Boss5": [],"Boss6": [],"jungle":[], "snow":[], "desert":[], "Boss7":[]}
     soundlist = {"zombie": pygame.mixer.Sound("sounds/hurt.wav"), 
                 "achievement": pygame.mixer.Sound("sounds/power.wav"), 
                 "coin": pygame.mixer.Sound("sounds/coin.wav"), 
