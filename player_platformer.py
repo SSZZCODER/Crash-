a13e56c3f8f55e51f5c50042571baedbe0f8133e
Sam Z <114539019+SSZZCODER@users.noreply.github.com>
2024-01-14 12:23:45 -0800

    jump

--- player_platformer.py
+++ player_platformer.py
@@ -1,12 +1,13 @@
 import pygame
 from pygame.math import Vector2
+import math
 
 class Player_Platformer:
     def __init__(self, x, y, speed, jumpheight, width, height, fist_width, fist_height):
         self.x = x
         self.y = y
         self.speed = speed
-        self.jumpheight = jumpheight
+        self.jumpheight = -jumpheight
         self.width = width
         self.height = height
         self.fist_width = fist_width
@@ -23,6 +24,9 @@ class Player_Platformer:
         self.fist_distance = 50
         self.fist_rect = pygame.Rect(0, 0, self.fist_width, self.fist_height)
         self.fist_rect.center = Vector2(self.rect.centerx+self.fist_distance * self.facing, self.rect.centery)
+        self.desiredjump = False
+        self.ontheground = False
+
         
     def move_x(self, keys):
         if keys[pygame.K_d]:
@@ -37,7 +41,9 @@ class Player_Platformer:
     def move_y(self, keys, dt):
         gravity = 5
         self.vel[1] += gravity*dt
-
+        if keys[pygame.K_w]:
+            if self.ontheground:
+                jumpvelocity = math.sqrt(-2*gravity*self.jumpheight)
     def collisions(self, platforms):
         self.dx = self.vel[0]
         self.dy = self.vel[1]
@@ -46,6 +52,10 @@ class Player_Platformer:
                 self.dy = platform.rect.top - self.rect.bottom
                 #self.dy = 0
                 self.vel[1] = 0 
+                self.ontheground = True
+                break
+            else:
+                self.ontheground = False
         self.rect.centerx += self.dx
         self.rect.centery += self.dy
         self.fist_rect.centerx = self.rect.centerx + self.fist_distance * self.facing
