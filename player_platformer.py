33ba49e39be4b493d530164668f99e7ca97bf8bc
izz13 <94653804+izz13@users.noreply.github.com>
2024-01-21 10:37:37 -0800

    added code for jumping

--- player_platformer.py
+++ player_platformer.py
@@ -41,21 +41,24 @@ class Player_Platformer:
     def move_y(self, keys, dt):
         gravity = 5
         self.vel[1] += gravity*dt
+        jumpvelocity = 0
         if keys[pygame.K_w]:
             if self.ontheground:
-                jumpvelocity = math.sqrt(-2*gravity*self.jumpheight)
+                self.ontheground = False
+                jumpvelocity = -1*math.sqrt(-2*gravity*self.jumpheight)
+                print(jumpvelocity)
+        self.vel[1] += jumpvelocity
+        
     def collisions(self, platforms):
         self.dx = self.vel[0]
         self.dy = self.vel[1]
         for platform in platforms:
             if platform.rect.colliderect(pygame.Rect(self.rect.x, self.rect.y + self.dy, self.rect.width, self.rect.height)):
                 self.dy = platform.rect.top - self.rect.bottom
-                #self.dy = 0
-                self.vel[1] = 0 
+                if self.dy < 0:
+                    self.dy = 0
+                self.vel[1] = 0
                 self.ontheground = True
-                break
-            else:
-                self.ontheground = False
         self.rect.centerx += self.dx
         self.rect.centery += self.dy
         self.fist_rect.centerx = self.rect.centerx + self.fist_distance * self.facing
@@ -65,9 +68,9 @@ class Player_Platformer:
         pygame.draw.rect(screen, (255, 0, 0), self.rect)
         pygame.draw.rect(screen, (255, 0, 0), self.fist_rect)
     def update(self, screen, keys, dt, platforms):
+        self.collisions(platforms)
         self.move_x(keys)
         self.move_y(keys, dt)
-        self.collisions(platforms)
         self.render(screen)
 
 
