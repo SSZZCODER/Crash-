a133e9519bc558aa723580aafe1382e977fd69e2
Sam Z <114539019+SSZZCODER@users.noreply.github.com>
2024-01-28 10:03:28 -0800

    boss players

--- bossplatformer.py
+++ bossplatformer.py
@@ -8,25 +8,36 @@ class Skarmy:
         self.height = height 
         self.damage = damage
         self.speed = 2
-        self.img = pygame.image.load("images/skeletonarmy.png")
+        self.health = 500
+        self.img_left = pygame.image.load("images/skeletonarmy.png")
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = [self.x, self.y]
-        self.img = pygame.transform.scale(self.img, [self.width, self.height])
+        self.img_left = pygame.transform.scale(self.img_left, [self.width, self.height])
+        self.facing = "Left"
+        self.img_right = pygame.transform.flip(self.img_left, 1, 0)
     def render(self, screen):
         self.rect.center = [self.x, self.y]
         pygame.draw.rect(screen, [0, 0, 0], self.rect)
-        screen.blit(self.img, self.rect)
+        if self.facing == "Left":
+            screen.blit(self.img_left, self.rect)
+        if self.facing == "Right":
+            screen.blit(self.img_right, self.rect)
     def move(self, player):
         vel_x = 0 
         if player.rect.left > self.rect.right:
+            self.facing = "Right"
             vel_x = self.speed
             if player.rect.left - self.rect.right < self.speed:
                 vel_x = player.rect.left - self.rect.right
         if player.rect.right < self.rect.left:
+            self.facing = "Left"
             vel_x = -self.speed
             if abs(player.rect.right - self.rect.left) < self.speed:
                 vel_x = player.rect.right - self.rect.left
         self.x += int(vel_x)
+    def healthbar(self, screen):
+        pygame.draw.rect(screen, [255, 0, 0], pygame.Rect(self.x-40, self.y-90, int((self.health/500)*80), 10))
     def update(self, screen, player):
         self.render(screen)
+        self.healthbar(screen)
         self.move(player)
