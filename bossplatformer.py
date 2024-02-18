0bd8176923acc24a1b6e3a80f94e3af7752baacb
Patrick Zhang <patkuanz@gmail.com>
2024-02-18 08:33:48 -0800

    newq images for boss

--- bossplatformer.py
+++ bossplatformer.py
@@ -15,6 +15,8 @@ class Skarmy:
         self.img_left = pygame.transform.scale(self.img_left, [self.width, self.height])
         self.state = "Left"
         self.img_right = pygame.transform.flip(self.img_left, 1, 0)
+        self.hit_left = pygame.image.load("images/skeletonarmy (3).png")
+        self.hit_right = pygame.image.load("images/skeletonarmy (4).png")
         self.washit = False
         self.washitcooldown = 5
         self.washitimer = self.washitcooldown
@@ -29,23 +31,23 @@ class Skarmy:
         if self.state == "Right":
             screen.blit(self.img_right, self.rect)
     def move(self, player):
-        if player.rect.left > self.rect.right:
-            self.state = "Right"
-            self.vel_x = self.speed
-            if abs(player.rect.left - self.rect.right) < self.speed:
-                print("stop")
-                self.vel_x = player.rect.left - self.rect.right
-                self.rect.right = player.rect.left
-        elif player.rect.right < self.rect.left:
-            self.state = "Left"
-            self.vel_x = -self.speed
-            if abs(player.rect.right - self.rect.left) < self.speed:
-                print("stop")
-                self.vel_x = player.rect.right - self.rect.left
-                self.rect.left = player.rect.right
+        if not self.washit:
+            if player.rect.left > self.rect.right:
+                self.state = "Right"
+                self.vel_x = self.speed
+                if abs(player.rect.left - self.rect.right) < self.speed:
+                    print("stop")
+                    self.vel_x = player.rect.left - self.rect.right
+                    self.rect.right = player.rect.left
+            elif player.rect.right < self.rect.left:
+                self.state = "Left"
+                self.vel_x = -self.speed
+                if abs(player.rect.right - self.rect.left) < self.speed:
+                    print("stop")
+                    self.vel_x = player.rect.right - self.rect.left
+                    self.rect.left = player.rect.right
         else:
-            if self.washit == False:
-                self.vel_x = 0
+            pass
         self.x += int(self.vel_x)
 
     def healthbar(self, screen):
@@ -60,9 +62,10 @@ class Skarmy:
                 self.washittimer = 0
                 print("player attacked boss")
                 if self.state == "Left":
-                    self.vel_x += self.pushback
+                    self.state = "hit_left"
                 if self.state == "Right":
-                    self.vel_x -= self.pushback
+                    self.state = "hit_right"
+
 
 
         if self.washit:
