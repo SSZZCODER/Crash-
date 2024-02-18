88f0d3b4cf31887d60e47192fd67bcde1f488d0b
Patrick Zhang <patkuanz@gmail.com>
2024-02-18 08:59:09 -0800

    boss hits

--- bossplatformer.py
+++ bossplatformer.py
@@ -16,12 +16,15 @@ class Skarmy:
         self.state = "Left"
         self.img_right = pygame.transform.flip(self.img_left, 1, 0)
         self.hit_left = pygame.image.load("images/skeletonarmy (3).png")
+        self.hit_left = pygame.transform.scale(self.hit_left, [self.width, self.height])
         self.hit_right = pygame.image.load("images/skeletonarmy (4).png")
+        self.hit_right = pygame.transform.scale(self.hit_right, [self.width, self.height])
         self.washit = False
         self.washitcooldown = 5
         self.washitimer = self.washitcooldown
         self.vel_x = 0
-        self.pushback = 50
+        self.pushback = 3
+        self.paddedstop = 100
 
     def render(self, screen):
         self.rect.center = [self.x, self.y]
@@ -30,25 +33,36 @@ class Skarmy:
             screen.blit(self.img_left, self.rect)
         if self.state == "Right":
             screen.blit(self.img_right, self.rect)
+        if self.state == "hit_left":
+            screen.blit(self.hit_left, self.rect)
+        if self.state == "hit_right":
+            screen.blit(self.hit_right, self.rect)
     def move(self, player):
         if not self.washit:
             if player.rect.left > self.rect.right:
                 self.state = "Right"
                 self.vel_x = self.speed
-                if abs(player.rect.left - self.rect.right) < self.speed:
-                    print("stop")
-                    self.vel_x = player.rect.left - self.rect.right
-                    self.rect.right = player.rect.left
+                #print(self.distancefromplayer(player))
+                if self.distancefromplayer(player) < self.paddedstop:
+                    self.vel_x = 0
             elif player.rect.right < self.rect.left:
                 self.state = "Left"
                 self.vel_x = -self.speed
-                if abs(player.rect.right - self.rect.left) < self.speed:
-                    print("stop")
-                    self.vel_x = player.rect.right - self.rect.left
-                    self.rect.left = player.rect.right
+                #print(self.distancefromplayer(player))
+                if self.distancefromplayer(player) < self.paddedstop:
+                    self.vel_x = 0
+                
         else:
-            pass
+            if self.state == "hit_left":
+                self.vel_x = self.pushback
+
+            elif self.state == "hit_right":
+                self.vel_x = -self.pushback
+
         self.x += int(self.vel_x)
+
+    def distancefromplayer(self, player):
+        return abs(self.rect.centerx - player.rect.centerx)
 
     def healthbar(self, screen):
         pygame.draw.rect(screen, [0, 0, 0], pygame.Rect(self.x-40, self.y-90, 80, 10))
